1002453b6a8f2d5d63c484fa91658c605fdfe96d
LxYx <1539357146@qq.com>
2023-10-22 20:23:09 +0800

    Configure allow CORS

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,14 @@
 import subprocess
 
 from fastapi import FastAPI, Body
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_methods=["*"],
+                   allow_headers=["*"])
 
 
 @app.post('/run')
